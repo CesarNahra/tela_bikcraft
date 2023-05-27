0612cfc184524e6110e9a1e1ec8688f43ed9e563
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-05-27 11:52:13 -0300

    Início tela bikcraft

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function changeTheme () {
+    const body = document.body;
+
+    const isLightTheme = body.classList.contains('light-theme');
+
+    body.classList.toggle('light-theme', !isLightTheme)
+}
